540316adaa8b3219109fd1115dfb919a1b126847
Дмитрий Опря < >
2017-02-27 00:22:03 +0200

    Add todos

--- client/app/components/todos.component.js
+++ client/app/components/todos.component.js
@@ -9,16 +9,39 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var todo_service_1 = require('../services/todo.service');
 var TodosComponent = (function () {
-    function TodosComponent() {
+    function TodosComponent(_todoService) {
+        this._todoService = _todoService;
     }
+    TodosComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.todos = [];
+        this._todoService.getTodos()
+            .subscribe(function (todos) {
+            _this.todos = todos;
+        });
+    };
+    TodosComponent.prototype.addTodo = function (event, todoText) {
+        var _this = this;
+        var result;
+        var newTodo = {
+            text: todoText.value,
+            isCompleted: false
+        };
+        result = this._todoService.saveTodo(newTodo);
+        result.subscribe(function (x) {
+            _this.todos.push(newTodo);
+            todoText.value = '';
+        });
+    };
     TodosComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'todos',
             templateUrl: 'todos.component.html'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [todo_service_1.TodoService])
     ], TodosComponent);
     return TodosComponent;
 }());
